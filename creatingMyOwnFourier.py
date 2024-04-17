2e923f49dcff29010bcac25c450470dc2b425ec7
ricardo <nocive.science@gmail.com>
2024-04-16 21:15:43 -0400

    reintetando con fourierCurve1.py

--- creatingMyOwnFourier.py
+++ creatingMyOwnFourier.py
@@ -9,29 +9,39 @@ class FourierScene(Scene):
         },
         'n_vectors': 10,
     }
+    
     def construct(self):
-        vectors= self.get_rotating_vectors()
+        vectors = self.get_rotating_vectors()
         self.add(vectors)
         self.wait(5)
+        
     def get_freqs(self):
-        return list(range(self.dicc['n_vectors']//2, -self.dicc['n_vectors']//2, -1))
+        all_freqs = list(range(self.dicc['n_vectors']//2, -self.dicc['n_vectors']//2, -1))
+        all_freqs.sort(key=abs)
+        return all_freqs
+    
     def get_coefficients(self):
         return [i*.1 for i in range(self.dicc['n_vectors'])]
+    
     def get_rotating_vector(self, position, coef, freq):
-        vector= Vector(RIGHT, **self.dicc['config_vector'])
+        vector = Vector(RIGHT, **self.dicc['config_vector'])
         vector.scale(coef)
-        vector.rotate(TAU*freq/self.dicc['n_vectors'], about_point=position)
         vector.shift(position)
+        vector.add_updater(lambda v, dt: self.update_vector(v, dt, freq))  # Actualiza el vector con una frecuencia dada
         return vector
-    def get_rotating_vectors(self,coefs=None, freqs=None):
-        vectors= VGroup()
-        last_vector_end= ORIGIN
+    
+    def update_vector(self, vector, dt, freq):
+        vector.rotate(TAU * freq * dt / self.dicc['n_vectors'], about_point=vector.get_start())  # Rota el vector con una frecuencia dada
+
+    def get_rotating_vectors(self, coefs=None, freqs=None):
+        vectors = VGroup()
+        start_position = ORIGIN
         if freqs is None:
-            freqs= self.get_freqs()
+            freqs = self.get_freqs()
         if coefs is None:
-            coefs= self.get_coefficients()
+            coefs = self.get_coefficients()
         for coef, freq in zip(coefs, freqs):
-            vector= self.get_rotating_vector(last_vector_end, coef, freq)
+            vector = self.get_rotating_vector(start_position, coef, freq)
             vectors.add(vector)
-            last_vector_end= vector.get_end()
+            start_position = vector.get_end()
         return vectors
---
+++ updateVector.py
@@ -0,0 +1,30 @@
+from manim import *
+class VectorScene(Scene):
+    dicc={
+        'vector_config': {
+            'buff': 0,
+            'max_tip_length_to_length_ratio': .2,
+            'stroke_width': .2
+        }
+    }
+    def construct(self):
+        vector = self.get_vector_rotating()
+        vector2 = self.get_vector_rotating_2()
+        self.add(vector, vector2)
+        self.wait(5)
+    def get_vector_rotating(self):
+        vector=self.vector = Vector(RIGHT, **self.dicc['vector_config'])
+        vector.add_updater(lambda v, dt: self.update_vector(v, dt))
+        return vector
+    def update_vector(self, vector, dt):
+        vector.rotate(TAU * dt / 2, about_point=vector.get_start())
+        return vector
+    def get_vector_rotating_2(self):
+        vector = Vector(RIGHT, **self.dicc['vector_config'])
+        mob = self.vector
+        vector.add_updater(lambda v, dt: self.update_vector_2(v, mob, dt))
+        return vector
+    def update_vector_2(self, vector, mob, dt):
+        vector.move_to(mob.get_end())
+        vector.rotate(TAU * dt / 2, about_point=vector.get_start())
+        return vector, mob
